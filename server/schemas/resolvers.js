90c9a90252dbad8eb033fb316ab35eb88acc5a1b
ahmadmujeeb33 <ahmadmujeeb333@gmail.com>
2021-07-28 17:36:18 -0400

    user resolvers added

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,72 +1,27 @@
+const { gql } = require('apollo-server-express');
 
-const { AuthenticationError } = require('apollo-server-express');
-const { User, Resume, Comments } = require('../models');
-const { signToken } = require('../utils/auth');
-const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
-
-const resolvers = {
-  Query: {
-    user: async (parent, args, context) => {
-      if (context.user) {
-        const user = await User.findById(context.user._id).populate({
-          path: 'orders.products',
-          populate: 'category'
-        });
-        user.orders.sort((a, b) => b.purchaseDate - a.purchaseDate);
-        return user;
-      }
-      throw new AuthenticationError('Not logged in');
-    },
-    resume: async (parent, args, context) => {
-      if (context.user) {
-        const resume = await Resume.find();
-        return resume;
-      }
-      throw new AuthenticationError('Not logged in');
-    }
-  },
-  Mutation: {
-    addUser: async (parent, args) => {
-      const user = await User.create(args);
-      const token = signToken(user);
-
-      return { token, user };
-    },
-    addOrder: async (parent, { products }, context) => {
-      console.log(context);
-      if (context.user) {
-        const order = new Order({ products });
-        await User.findByIdAndUpdate(context.user._id, { $push: { orders: order } });
-        return order;
-      }
-      throw new AuthenticationError('Not logged in');
-    },
-    updateUser: async (parent, args, context) => {
-      if (context.user) {
-        return await User.findByIdAndUpdate(context.user._id, args, { new: true });
-      }
-
-      throw new AuthenticationError('Not logged in');
-    },
-    login: async (parent, { email, password }) => {
-      const user = await User.findOne({ email });
-
-      if (!user) {
-        throw new AuthenticationError('Incorrect credentials');
-      }
-
-      const correctPw = await user.isCorrectPassword(password);
-
-      if (!correctPw) {
-        throw new AuthenticationError('Incorrect credentials');
-      }
-
-      const token = signToken(user);
-
-      return { token, user };
-    }
+const typeDefs = gql`
+  type User {
+    _id: ID
+    userName: String
+    email: String
+    password: String
   }
-};
 
-module.exports = resolvers;
+  type Auth {
+    token: ID!
+    user: User
+  }
 
+  type Query {
+    users: [User]!
+    user(userId: ID!): User
+  }
+
+  type Mutation {
+    addUser(name: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+  }
+`;
+
+module.exports = typeDefs;
