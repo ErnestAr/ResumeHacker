379bd4e688e2af183dcbb08b8f2aa100b78a24fc
ahmadmujeeb33 <ahmadmujeeb333@gmail.com>
2021-07-28 16:38:38 -0400

    user seeds updated

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -5,20 +5,13 @@ db.once('open', async () => {
   await User.deleteMany();
 
   await User.create({
-    firstName: 'Pamela',
-    lastName: 'Washington',
+    userName: 'Pamela',
     email: 'pamela@testmail.com',
     password: 'password12345',
-    orders: [
-      {
-        products: [products[0]._id, products[0]._id, products[1]._id]
-      }
-    ]
   });
 
   await User.create({
-    firstName: 'Elijah',
-    lastName: 'Holt',
+    userName: 'Elijah',
     email: 'eholt@testmail.com',
     password: 'password12345'
   });
