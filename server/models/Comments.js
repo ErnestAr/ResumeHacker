b31dc2a987ad5920335e82cb68418f0f3716d1c9
ErnestAr <arutiunian.ernest@gmail.com>
2021-07-28 11:00:43 -0400

    models

---
+++ server/models/Comments.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const resumeSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  content: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  user: {
+    type: Schema.ObjectId,
+    ref: 'User'
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Resume = mongoose.model('Resume', resumeSchema);
+
+module.exports = Resume;
