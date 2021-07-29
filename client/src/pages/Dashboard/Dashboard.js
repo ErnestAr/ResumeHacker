9c5e74b02b959d17f3d62dc431e1a2061ef39195
ErnestAr <arutiunian.ernest@gmail.com>
2021-07-29 08:36:03 -0400

    dashboard basic styling

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -4,14 +4,16 @@ import axios from 'axios';
 import { saveAs } from 'file-saver';
 
 import "./dashboard.css"
+import {VscAccount} from "react-icons/vsc"
+
 
 
 class Dashboard extends Component {
   state = {
-    name: '',
-    receiptId: 0,
-    price1: 0,
-    price2: 0,
+    FirstName: '',
+    LastName: '',
+    Cell: '',
+    Email: '',
   }
 
   handleChange = ({ target: { value, name }}) => this.setState({ [name]: value })
@@ -26,14 +28,55 @@ class Dashboard extends Component {
       })
   }
 
+  // name="name" onChange={this.handleChange} <input type="number" placeholder="Receipt ID" name="receiptId" onChange={this.handleChange} />
+
   render() {
     return (
-      <div className="Dashboard">
-        <input type="text" placeholder="Name" name="name" onChange={this.handleChange}/>
-        <input type="number" placeholder="Receipt ID" name="receiptId" onChange={this.handleChange} />
-        <input type="number" placeholder="Price 1" name="price1" onChange={this.handleChange} />
-        <input type="number" placeholder="Price 2" name="price2" onChange={this.handleChange} />
-        <button onClick={this.createAndDownloadPdf}>Download PDF</button>
+      <div className="dashboard row mt-5 p-3">
+          <aside className="col-3 bd-highlight  ">
+            <div className="col-10">
+              <VscAccount className="p-2 mb-3 m-1" size="275px"/>
+              <h3 className="text-center" >Username</h3>
+              <h3>Email</h3>
+            </div>
+            <div  className="col-10 mt-3">
+              <h3 className="text-center">Acoount Settings</h3> 
+              <h3 className="text-center">Update Information</h3>
+              <h3 className="text-center">Delete Sccount</h3>
+            </div>
+          </aside>
+          <main className=" col-9 wm">
+              <section className="  row bd-highlight p-2 bg-color rounded">
+                <h2 className="p-1">Create a Template</h2>
+                <div className="col-3 p-2">
+                  <img src="/images/template.png" alt="template" className="img-fluid" />
+                </div>
+                <div className="col-3 p-2">
+                  <img src="/images/template.png" alt="template" className="img-fluid" />
+                </div>
+                <div className="col-3 p-2">
+                  <img src="/images/template.png" alt="template" className="img-fluid" />
+                </div>
+                <div className="col-3 p-2">
+                  <img src="/images/template.png" alt="template" className="img-fluid" />
+                </div>
+              </section>
+              <section className=" mt-5 row bd-highlight p-2 bg-color rounded">
+                <h2 className="p-1">Your Resume Templates</h2>
+                <div className="col-3 p-2">
+                  <img src="/images/template.png" alt="template" className="img-fluid" />
+                </div>
+                <div className="col-3 p-2">
+                  <img src="/images/template.png" alt="template" className="img-fluid" />
+                </div>
+                <div className="col-3 p-2">
+                  <img src="/images/template.png" alt="template" className="img-fluid" />
+                </div>
+                <div className="col-3 p-2">
+                  <img src="/images/template.png" alt="template" className="img-fluid" />
+                </div>
+              </section>
+          </main>
       </div>
     );
   }
