2e7a38c265d401534e8d088b9c953f1679779805
ahmadmujeeb33 <ahmadmujeeb333@gmail.com>
2021-07-29 20:55:20 -0400

    had to update this

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,44 +1,27 @@
-const { AuthenticationError } = require('apollo-server-express');
-const { User } = require('../models');
-const { signToken } = require('../utils/auth');
+const { gql } = require('apollo-server-express');
 
-const resolvers = {
-    Query: {
-        users: async () => {
-          return User.find();
-        },
-    
-        user: async (parent, { userId }) => {
-          return User.findOne({ _id: userId });
-        },
-    },
+const typeDefs = gql`
+  type User {
+    _id: ID!
+    userName: String
+    email: String
+    password: String
+  }
 
-    Mutation: {
-        addUser: async (parent, { userName, email, password }) => {
-          const user = await User.create({ userName, email, password });
-          const token = signToken(user);
-    
-          return { token, user };
-        },
-        login: async (parent, { userName, password }) => {
-          const user = await User.findOne({ userName });
-    
-          if (!user) {
-            throw new AuthenticationError('No profile with this email found!');
-          }
-    
-          const correctPw = await user.isCorrectPassword(password);
-    
-          if (!correctPw) {
-            throw new AuthenticationError('Incorrect password!');
-          }
-    
-          const token = signToken(user);
-          return { token, user };
-        },
-    
-        
-    },
-}
+  type Auth {
+    token: ID!
+    user: User
+  }
 
-module.exports = resolvers;
+  type Query {
+    users: [User]
+    user(userId: ID!): User
+  }
+  
+  type Mutation {
+    addUser(userName: String!, email: String!, password:String!): Auth
+    login(userName: String!, password: String!): Auth
+  }
+`;
+
+module.exports = typeDefs;
