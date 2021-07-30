49f3b5b9d5b1cad847ae6cb13122e6b6ee0be8d0
ahmadmujeeb33 <ahmadmujeeb333@gmail.com>
2021-07-29 21:34:10 -0400

    added conditional for switching between login and logout

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,16 +5,35 @@ import Nav from 'react-bootstrap/Nav'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../App.css'
 
+import Auth from '../utils/auth';
+
+const logout = (event) => {
+    event.preventDefault();
+    Auth.logout();
+  };
+
 
 function theNavbar() {
+    console.log(Auth.loggedIn());
     return (
         <Navbar>
             <Container >
                 <Nav className="me-auto">
-                    <Nav.Link className='navbar-link' href="/"><span>Home</span></Nav.Link>
-                    <Nav.Link className='navbar-link' href="/Dashboard"><span>Dashboard</span></Nav.Link>
-                    <Nav.Link className='navbar-link' href="/About"><span>About</span></Nav.Link>
-                    <Nav.Link className='navbar-link' href="/LoginSignUp"><span>Login/Signup</span></Nav.Link>
+                {Auth.loggedIn() ? (
+                    <div>
+                        <Nav.Link className='navbar-link' href="/"><span>Home</span></Nav.Link>
+                        <Nav.Link className='navbar-link' href="/Dashboard"><span>Dashboard</span></Nav.Link>
+                        <Nav.Link className='navbar-link' href="/About"><span>About</span></Nav.Link>
+                        <button className="btn btn-lg btn-light m-2" onClick={logout}>Logout</button>
+                    </div>
+                ) : (
+                    <div>
+                        <Nav.Link className='navbar-link' href="/"><span>Home</span></Nav.Link>
+                        <Nav.Link className='navbar-link' href="/LoginSignUp"><span>Dashboard</span></Nav.Link>
+                        <Nav.Link className='navbar-link' href="/About"><span>About</span></Nav.Link>
+                        <Nav.Link className='navbar-link' href="/LoginSignUp"><span>Login/Signup</span></Nav.Link>
+                    </div>
+                )}      
                 </Nav>
             </Container>
         </Navbar>
