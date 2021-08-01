b6bedec7a9178fac3e8bc92c5207beede91b9662
ahmadmujeeb33 <ahmadmujeeb333@gmail.com>
2021-08-01 16:27:34 -0400

    password blank now empty

--- client/src/components/UpdateUser.js
+++ client/src/components/UpdateUser.js
@@ -85,7 +85,7 @@ export default function UpdateUser() {
                     <input   onChange = {SignUpChange}
                         name="password"
                         type="password"
-                        value={formState.password}
+                        
                     > 
                     </input> 
 
