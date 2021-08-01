8fc4b987aae833c52044b8457701efd4568fa24d
ahmadmujeeb33 <ahmadmujeeb333@gmail.com>
2021-08-01 15:23:49 -0400

    changes the name of function

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -6,6 +6,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '../App.css'
 
 import Auth from '../utils/auth';
+import { QUERY_ME } from '../utils/queries';
+import { useQuery } from '@apollo/client';
+
+
 
 const logout = (event) => {
     event.preventDefault();
@@ -13,17 +17,17 @@ const logout = (event) => {
   };
 
 
-function theNavbar() {
-    console.log(Auth.loggedIn());
-    return (
 
+function TheNavbar() {
+    
+    return (
         <Navbar>
             <Container >
                 <Nav className="me-auto">
                 {Auth.loggedIn() ? (
                     <div>
                         <Nav.Link className='navbar-link' href="/"><span>Home</span></Nav.Link>
-                        <Nav.Link className='navbar-link' href="/Dashboard"><span>Dashboard</span></Nav.Link>
+                        <Nav.Link className='navbar-link' href={"/Dashboard"}><span>Dashboard</span></Nav.Link>
                         <Nav.Link className='navbar-link' href="/About"><span>About</span></Nav.Link>
                         <button className="btn btn-lg btn-light m-2" onClick={logout}>Logout</button>
                     </div>
@@ -42,4 +46,4 @@ function theNavbar() {
     );
 }
 
-export default theNavbar;
+export default TheNavbar;
