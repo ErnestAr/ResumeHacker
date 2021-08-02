9f7c2c2b61d725181af42604dd0d83af55876b7d
ErnestAr <arutiunian.ernest@gmail.com>
2021-08-01 21:55:28 -0400

    new Cards

--- client/src/components/Templates.js
+++ client/src/components/Templates.js
@@ -37,33 +37,90 @@ import ImageListItemBar from '@material-ui/core/ImageListItemBar';
 import ListSubheader from '@material-ui/core/ListSubheader';
 import IconButton from '@material-ui/core/IconButton';
 import InfoIcon from '@material-ui/icons/Info';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
 // onClick={handleClick}
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 500,
+  },
+  media: {
+    height: 500,
+  },
+});
+
 
 export default function Templates() {
   const history = useHistory();
   function handleClick() {
     history.push("/dashboard/resume");
   }
+  const classes = useStyles();
     return (
-            <main className=" col-9 wm">
-              <section className="  row p-2  card ">
-              <h2 className="p-1">Starter guide</h2>
-              </section>
-              <section className="row bd-highlight p-2 card">
-                <h2 className="p-1">Select Template</h2>
-              </section>
-              <Grid container spacing={3}>
-                <Grid item xs={6} className="" >
-                  <h4 >Template 1</h4>
-                  <img src="/images/templategreen.png" alt="template2" className="img-fluid tempimg card-1" onClick={handleClick}/>
-                  <div></div>
+              <main className=" col-9 wm">
+                <section className="  row p-2  card ">
+                <h2 className="p-1">Starter guide</h2>
+                </section>
+                <section className="row bd-highlight p-2 card">
+                  <h2 className="p-1">Select Template</h2>
+                </section>
+                <Grid container spacing={3}>
+                  <Grid item xs={6} className="" >
+                    <Card className={classes.root}>
+                      <CardActionArea>
+                        <CardMedia
+                          className={classes.media}
+                          image="/images/templategreen.png"
+                          title="Contemplative Reptile"
+                        />
+                        <CardContent>
+                          <Typography gutterBottom variant="h5" component="h2">
+                            Software Developer
+                          </Typography>
+                          <Typography variant="body2" color="textSecondary" component="p">
+                            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                            across all continents except Antarctica
+                          </Typography>
+                        </CardContent>
+                      </CardActionArea>
+                      <CardActions>
+                        <Button size="small" color="primary" onClick={handleClick}>
+                          Create Resume
+                        </Button>
+                      </CardActions>
+                    </Card>
+                  </Grid>
+                  <Grid item xs={6} className="" >
+                    <Card className={classes.root}>
+                        <CardActionArea>
+                          <CardMedia
+                            className={classes.media}
+                            image="/images/templateblue.png"
+                            title="Contemplative Reptile"
+                          />
+                          <CardContent>
+                            <Typography gutterBottom variant="h5" component="h2">
+                              Software Developer (Entry Level)
+                            </Typography>
+                            <Typography variant="body2" color="textSecondary" component="p">
+                              Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                              across all continents except Antarctica
+                            </Typography>
+                          </CardContent>
+                        </CardActionArea>
+                        <CardActions>
+                          <Button size="small" color="primary" onClick={handleClick}>
+                            Create Resume
+                          </Button>
+                        </CardActions>
+                      </Card>
+                  </Grid>
                 </Grid>
-                <Grid item xs={6} className="" >
-                  <h4 >Template 2</h4>
-                  <img src="/images/templateblue.png" alt="template2" className="img-fluid tempimg card-1" />
-                  <div></div>
-                </Grid>
-              </Grid>
-          </main>
+              </main>
     )
 }
