f2e058451c15c5a4f25af7b8c7904bc8245071d5
ErnestAr <arutiunian.ernest@gmail.com>
2021-08-03 11:20:29 -0400

    heroku update

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,7 +4,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../App.css'
-
+import {Link} from 'react-router-dom';
 import Auth from '../utils/auth';
 import { QUERY_ME } from '../utils/queries';
 import { useQuery } from '@apollo/client';
@@ -18,7 +18,6 @@ const logout = (event) => {
   };
 
 
-
 function TheNavbar() {
     
     return (
@@ -28,18 +27,17 @@ function TheNavbar() {
                 {Auth.loggedIn() ? (
 
                     <div className='d-flex'>
-                        <Nav.Link className='navbar-link mt-2' href="/"><span className='nav-text' style={{color:'#3A7CA5', fontSize:'1.5em'}}>Home</span></Nav.Link>
-                        <Nav.Link className='navbar-link mt-2' href="/Dashboard"><span className='nav-text' style={{color:'#3A7CA5', fontSize:'1.5em'}}>Dashboard</span></Nav.Link>
-                        <Nav.Link className='navbar-link mt-2' href="/About"><span className='nav-text' style={{color:'#3A7CA5', fontSize:'1.5em'}}>About</span></Nav.Link>
+                        <Link className='navbar-link mt-2' to="/"><span className='nav-text' style={{color:'#3A7CA5', fontSize:'1.5em'}}>Home</span></Link>
+                        <Link className='navbar-link mt-2' to="/Dashboard"><span className='nav-text' style={{color:'#3A7CA5', fontSize:'1.5em'}}>Dashboard</span></Link>
+                        <Link className='navbar-link mt-2' to="/About"><span className='nav-text' style={{color:'#3A7CA5', fontSize:'1.5em'}}>About</span></Link>
                         <button className="btn btn-lg btn-primary m-2 navBtn" onClick={logout}>Logout</button>
-
                     </div>
                 ) : (
                     <div className='d-flex flex-row'>
-                        <Nav.Link className='navbar-link' href="/"><span className='nav-text' style={{color:'#3A7CA5', fontSize:'1.5em'}}>Home</span></Nav.Link>
-                        <Nav.Link className='navbar-link' href="/LoginSignUp"><span className='nav-text' style={{color:'#3A7CA5', fontSize:'1.5em'}}>Dashboard</span></Nav.Link>
-                        <Nav.Link className='navbar-link' href="/About"><span className='nav-text' style={{color:'#3A7CA5', fontSize:'1.5em'}}>About</span></Nav.Link>
-                        <Nav.Link className='navbar-link' href="/LoginSignUp"><span className='nav-text' style={{color:'#3A7CA5', fontSize:'1.5em'}}>Login/Signup</span></Nav.Link>
+                        <Link className='navbar-link' to="/"><span className='nav-text' style={{color:'#3A7CA5', fontSize:'1.5em'}}>Home</span></Link>
+                        <Link className='navbar-link' to="/LoginSignUp"><span className='nav-text' style={{color:'#3A7CA5', fontSize:'1.5em'}}>Dashboard</span></Link>
+                        <Link className='navbar-link' to="/About"><span className='nav-text' style={{color:'#3A7CA5', fontSize:'1.5em'}}>About</span></Link>
+                        <Link className='navbar-link' to="/LoginSignUp"><span className='nav-text' style={{color:'#3A7CA5', fontSize:'1.5em'}}>Login/Signup</span></Link>
                     </div>
                 )}      
                 </Nav>
